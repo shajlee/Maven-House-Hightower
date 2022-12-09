e7794eda4c57674a81245e72810a1ebac871ca1f
shajlee <chowdhurygs@gmail.com>
2022-12-08 18:27:54 -0800

    first commit

---
+++ src/test/java/house_hightower_jenkins_project/Methods_Classes_URL_Validation.java
@@ -0,0 +1,41 @@
+package house_hightower_jenkins_project;
+
+public class Methods_Classes_URL_Validation {
+
+	public static void main(String[] args) {
+		Methods_Classes_URL_Validation.OttoHightower();
+		Methods_Classes_URL_Validation.AlicentHightower();
+		
+		Methods_Classes_URL_Validation HouseHightower = new Methods_Classes_URL_Validation();
+		HouseHightower.AegonIITargaryen();
+		HouseHightower.AemondTargaryen();
+		
+		Methods_Classes_URL_Validation HouseHightower1 = new Methods_Classes_URL_Validation();
+		HouseHightower1.DaeronTargaryen();
+		
+	
+	}
+	public static void OttoHightower() {
+		System.out.println("Hand of the King");
+	}
+	public static void AlicentHightower() {
+		System.out.println("The Queen in Chains");
+	}
+	public static void AegonIITargaryen() {
+		System.out.println("Aegon the Elder");
+	}
+	public static void AemondTargaryen() {
+		System.out.println("Aemond the one eye");
+	}
+	public static void DaeronTargaryen() {
+		System.out.println("Daeron the Daring");
+		
+		
+	
+
+		
+	
+
+	}
+
+}
